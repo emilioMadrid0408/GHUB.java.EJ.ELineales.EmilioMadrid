01983342536170c895e1cd7a422179c8492108ae
Juan Emilio Madrid Alvarez <158490593+emilioMadrid0408@users.noreply.github.com>
2024-03-12 15:57:56 +0000

    pila

--- miPrincipal/Nodo.java
+++ miPrincipal/Nodo.java
@@ -2,5 +2,29 @@ package miPrincipal;
 
 public class Nodo<T> {
 
-    
+    private T valor;
+
+    Nodo<T> siguiente;
+
+    public Nodo() {
+        valor = null;
+        siguiente = null;
+    }
+
+    public T getValor() {
+        return valor;
+    }
+
+    public void setValor(T valor) {
+        this.valor = valor;
+    }
+
+    public Nodo<T> getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo<T> siguiente) {
+        this.siguiente = siguiente;
+    }
+
 }
---
+++ miPrincipal/Pila.java
@@ -0,0 +1,32 @@
+package miPrincipal;
+
+public class Pila<T> {
+
+    private Nodo<T> cabeza;
+    private int tamanio;
+
+    public Pila() {
+        cabeza = null;
+        tamanio = 0;
+    }
+
+    public int getTamanio() {
+        return tamanio;
+    }
+
+    public boolean esVacia() {
+        return (cabeza == null);
+    }
+
+    public void apilar(T valor) {
+        Nodo<T> nuevo = new Nodo<T>();
+        nuevo.setValor(valor);
+        if (esVacia()) {
+            cabeza = nuevo;
+        } else {
+            nuevo.setSiguiente(cabeza);
+            cabeza = nuevo;
+        }
+        tamanio++;
+    }
+}
